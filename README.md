f6402599aec5b6c3ea4c0680281806d2980fb0fc
Alexandre Bourget <alex@bourget.cc>
2019-05-17 11:23:49 -0400

    First draft of the Data Integrity Proof Protocol.

---
+++ README.md
@@ -0,0 +1,5 @@
+Data Integrity Proof Protocol
+=============================
+
+Paired with our Data Integrity Bounty Program, this is for a proof
+that we emitted this or that data.
---
+++ dipp.go
@@ -0,0 +1,69 @@
+package dipp
+
+import (
+	"encoding/hex"
+	"net/http"
+
+	"golang.org/x/crypto/sha3"
+)
+
+func IntegrityProofMiddleware(secret string, handler http.Handler) http.Handler {
+	return &ProofMiddleware{next: handler, secret: secret}
+}
+
+// ProofMiddleware will hash the response and add a header. NOTE that
+// this middleware will break Trailers added by calling code, after
+// their WriteHeader() call.
+type ProofMiddleware struct {
+	next   http.Handler
+	secret string
+}
+
+func (m *ProofMiddleware) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if r.Header.Get("X-Data-Integrity-Proof") == "true" {
+		writer := &ProofWriter{
+			ResponseWriter: w,
+		}
+
+		m.next.ServeHTTP(writer, r)
+
+		proof := mhashup(m.secret, writer.buffer)
+		w.Header().Set("X-Data-Integrity-Proof", proof)
+
+		if writer.code != 0 {
+			w.WriteHeader(writer.code)
+		}
+
+		_, _ = writer.Write(writer.buffer)
+	} else {
+		m.next.ServeHTTP(w, r)
+	}
+}
+
+type ProofWriter struct {
+	http.ResponseWriter
+
+	code int
+
+	buffer []byte
+}
+
+func (w *ProofWriter) Write(data []byte) (int, error) {
+	w.buffer = append(w.buffer, data...)
+	return len(data), nil
+}
+
+func (w *ProofWriter) WriteHeader(code int) {
+	w.code = code
+}
+
+func mhashup(secret string, payload []byte) string {
+	digest := sha3.NewShake256()
+	_, _ = digest.Write([]byte(secret))
+	_, _ = digest.Write(payload)
+
+	out := make([]byte, 32)
+	_, _ = digest.Read(out)
+
+	return hex.EncodeToString(out)
+}
